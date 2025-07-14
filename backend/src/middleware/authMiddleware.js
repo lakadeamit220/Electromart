1cbf7b37bfbfd7a93d94881c743f2439e905d375
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-07-14 18:07:10 +0530

    Implemented server.js(entry point of the backend.)

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+  const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
 
   try {
