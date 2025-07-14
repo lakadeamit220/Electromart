b2f366c0dbd4d748aa063c914c719986a657acf2
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-07-14 16:30:12 +0530

    Implemented Product model.

---
+++ backend/src/models/Product.js
@@ -0,0 +1,12 @@
+import { Schema, model } from 'mongoose';
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  stock: { type: Number, required: true, default: 0 },
+});
+
+export default model('Product', productSchema);
