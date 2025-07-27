70c02012d6acd93c224eb31ce84841f104607af3
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-07-27 13:16:26 +0530

    modified authStore.

--- frontend/src/store/authStore.jsx
+++ frontend/src/store/authStore.jsx
@@ -1,10 +1,15 @@
 import { create } from "zustand";
+import { toast } from "react-toastify";
 
 const useAuthStore = create((set) => ({
   user: null,
   token: null,
   setUser: (user, token) => set({ user, token }),
-  logout: () => set({ user: null, token: null }),
+  updateUser: (user) => set((state) => ({ ...state, user })),
+  logout: () => {
+    toast.info("Logged out successfully");
+    return { user: null, token: null };
+  },
 }));
 
 export default useAuthStore;
