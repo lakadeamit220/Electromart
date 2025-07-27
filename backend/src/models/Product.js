4d4c662fd927586fdccf8a83b79a3a433e81a06b
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-07-27 13:03:51 +0530

    updated Product model.

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -7,6 +7,7 @@ const productSchema = new mongoose.Schema({
   category: { type: String, required: true },
   image: { type: String, required: true },
   stock: { type: Number, required: true, default: 0 },
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
 });
 
 export default mongoose.model('Product', productSchema);
